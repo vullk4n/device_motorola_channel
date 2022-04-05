1cd8f166b564ee777b60d7299597b7186050f010
Andrew Hexen <SyberHexen@gmail.com>
2022-04-05 07:34:41 -0700

    channel: add some missing audio files
    * after change I102d96b43f3adab5d717ae1512164d6e98f79028 these files were dropped from common.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Id5e82a528e534df693408edf80ac3b20999d0583

--- device.mk
+++ device.mk
@@ -42,6 +42,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_tas2560.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_tas2560.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
